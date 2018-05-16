3ba8f65ba2434398825f33332ebdbbf2bbbf5f3f
bass <bass@exciteholidays.com>
2018-05-16 10:22:00 +0700

    change message v1

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World ON AWS CODE PIPELINE by Bass!";
+        return "Hello World ON AWS CODE PIPELINE by Bass Srongsil!";
     }
 
 	@RequestMapping(value="/print/{message}", method=RequestMethod.GET)

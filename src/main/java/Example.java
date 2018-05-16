3b23d77f6afaaa153d8e0b704323908e77b48572
bass <bass@exciteholidays.com>
2018-05-16 10:48:16 +0700

    test 1

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World ON AWS CODE PIPELINE by Bass Srongsil!";
+        return "Hello World ON AWS CODE PIPELINE by Bass Srongsil V.1 !";
     }
 
 	@RequestMapping(value="/print/{message}", method=RequestMethod.GET)
